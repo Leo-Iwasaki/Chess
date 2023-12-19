35cf2c4b9355b29a8d4b93c378e9764f0538699e
Kaede Sugano <ks2222@ic.ac.uk>
2023-12-19 17:24:55 +0000

    initialised xcode proj

---
+++ Chess/Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Kaede Sugano on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

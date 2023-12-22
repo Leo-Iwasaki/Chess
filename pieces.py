1768a16e21d94aa921f1ca4ed9e2b673c63fea17
ym2522 <ym2522@ic.ac.uk>
2023-12-22 16:27:55 +0000

    Board.__move()

--- pieces.py
+++ pieces.py
@@ -1,48 +1,66 @@
+from chess import BOARD_WIDTH
+
 class Piece:
-    def __init__(self, colour, name, tile):
+    def __init__(self, colour, name, coord):
         self.__colour = colour
         self.__name = name
-        self.__tile = tile
+        self.__coord = coord
         self.__next_valid_moves = []
-        
+
+    def get_colour(self):
+        return self.__colour
+
     def get_name(self):
         return self.__name
     
-#functions below should be rewritten for each piece type
+    # Return a list of moves as tuples (from, to)
     def move(self, colour, end):
-        if colour != self.__colour:
-            return []
-        if end in self.__next_valid_moves:
-            #move
-            return [] #return a list of moves as tuples (from, to)
-        return []
+        ret = []
+        if colour == self.__colour and end in self.__next_valid_moves:
+            ret.append((self.__coord, end))
+        return ret
+
+    def set_coord(self, coord):
+        self.__coord = coord
     
     def valid_moves(self, pieces, checking_pieces, pinning):
-        #set self.__next_valid_moves to a list of valid tiles to move to.
+        #set self.__next_valid_moves to a list of valid coords to move to.
         return
 
 class Pawn(Piece):
-    def __init__(self, colour, tile):
-        super().__init__(colour, "p", tile)
+    def __init__(self, colour, coord):
+        super().__init__(colour, "p", coord)
 
 class Knight(Piece):
-    def __init__(self, colour, tile):
-        super().__init__(colour, "n", tile)
+    def __init__(self, colour, coord):
+        super().__init__(colour, "n", coord)
 
 class Bishop(Piece):
-    def __init__(self, colour, tile):
-        super().__init__(colour, "b", tile)
+    def __init__(self, colour, coord):
+        super().__init__(colour, "b", coord)
 
 class Rook(Piece):
-    def __init__(self, colour, tile):
-        super().__init__(colour, "r", tile)
+    def __init__(self, colour, coord):
+        super().__init__(colour, "r", coord)
 
 class Queen(Piece):
-    def __init__(self, colour, tile):
-        super().__init__(colour, "q", tile)
-        self.__rook = Rook(colour, tile)
-        self.__bishop = Bishop(colour, tile)
+    def __init__(self, colour, coord):
+        super().__init__(colour, "q", coord)
+        self.__rook = Rook(colour, coord)
+        self.__bishop = Bishop(colour, coord)
 
 class King(Piece):
-    def __init__(self, colour, tile):
-        super().__init__(colour, "k", tile)
+    def __init__(self, colour, coord):
+        super().__init__(colour, "k", coord)
+    
+    # Return a list of moves as tuples (from, to)
+    # Override for castle
+    def move(self, colour, end):
+        ret = []
+        if colour == self.__colour and end in self.__next_valid_moves:
+            ret.append((self.__coord, end))
+            if end[1] == self.__coord[1] - 2:
+                ret.append(((end[0], 0), (end[0], end[1] + 1)))
+            elif end[1] == self.__coord[1] + 2:
+                ret.append(((end[0], BOARD_WIDTH - 1), (end[0], end[1] - 1)))
+        return ret
